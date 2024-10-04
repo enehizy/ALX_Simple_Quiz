d2f48f0e1a9d033d79adacb9174977df2b2bf918
enehizy <osamudiameenehizena@gmail.com>
2024-10-04 23:05:07 +0100

    radio quiz

---
+++ quiz.js
@@ -0,0 +1,16 @@
+function checkAnswer() {
+    // Function body
+    let feedback =document.getElementById("feedback");
+    let correctAnswer ="4";
+    const selectedRadio = document.querySelector('input[name="quiz"]:checked');
+    let userAnswer =selectedRadio.value;
+  
+    if(userAnswer == correctAnswer){
+        feedback.textContent ="Correct! Well done.";
+    }else{
+        feedback.textContent ="That's incorrect. Try again!";
+    }
+}
+
+let button = document.getElementById("submit-answer");
+button.addEventListener("click",checkAnswer,true);
